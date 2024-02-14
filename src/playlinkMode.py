61bb93b01aceef6eb49fc7a12d404629299513fd
MidVC <lutony040720@gmail.com>
2024-02-13 23:37:10 -0500

    implemented playlinkMode

--- src/playlinkMode.py
+++ src/playlinkMode.py
@@ -1,10 +1,22 @@
 import os
-from ossapi import Ossapi, Beatmap
+from ossapi import Ossapi, Score, Beatmap, User, Mods
+import handlers
 
 clientId = os.getenv("OAUTHCLIENTID")
 clientSecret = os.getenv("OAUTHCLIENTSECRET")
 api = Ossapi(clientId, clientSecret)
 
+def handlePlaylinkMode(playId: int) -> str:
 
-def handlePlaylinkMode():
-    pass
+    # Get the score and IDs we need
+    score: Score = api.score(score_id=playId)
+    beatmapId = score.beatmap.id
+    playerId = score.user_id
+    mods = score.mods.short_name()
+
+    return handlers.handlePlay(api, playId) + "\n" + \
+        handlers.handlePlayer(api, playerId) + "\n" + \
+        handlers.handleSkin(api, mods) + "\n" + \
+        handlers.handleMap(api, beatmapId)
+        
+
