d731be3da429dbae7841d21c4715316534b0c242
MidVC <lutony040720@gmail.com>
2024-02-19 17:09:56 -0500

    implemented handlePlay

--- src/handlers/handlePlay.py
+++ src/handlers/handlePlay.py
@@ -1,4 +1,6 @@
 from ossapi import Ossapi, Score
 
-def handlePlay(api:Ossapi, score:Score, rank:int) -> str:
-    return ""
+def handlePlay(api:Ossapi, score:Score) -> str:
+    return "#" + str(score.rank_global) +\
+        " global (#" + str(score.rank_country) + " CN) map rank, " +\
+        "{:.2f}".format(score.pp) + "pp\n"
