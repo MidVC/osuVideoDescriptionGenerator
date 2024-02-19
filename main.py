e1849ba64be6301fe68dc9935af654e4eb6ba6f9
MidVC <lutony040720@gmail.com>
2024-02-19 17:41:40 -0500

    implemented main.py

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import os
+import sys
 from dotenv import load_dotenv
+from .src.beatmapMode import handleBeatmapMode
+from .src.playlinkMode import handlePlaylinkMode
 
 load_dotenv('.env.example')
 
 if __name__ == '__main__':
 
+    mode = os.environ.get('MODE')
 
+    if mode == 1:
+        handleBeatmapMode()
+    elif mode == 2:
+        print("Playlink mode not available", file=sys.stderr)
+        exit(0)
 
-    
+        handlePlaylinkMode()
+    else:
+        print("Wrong mode provided in .env!", file=sys.stderr)
+        exit(0)
