d65d67e8022e7cd15e57e84af4c8f5831a30bcbe
MidVC <lutony040720@gmail.com>
2024-02-19 17:23:06 -0500

    fixed typo in handlePlay

--- src/handlers/handlePlay.py
+++ src/handlers/handlePlay.py
@@ -2,5 +2,5 @@ from ossapi import Ossapi, Score
 
 def handlePlay(api:Ossapi, score:Score) -> str:
     return "#" + str(score.rank_global) +\
-        " global (#" + str(score.rank_country) + " CN) map rank, " +\
+        " global (#" + str(score.rank_country) + " " + score.user().country_code + ") map rank, " +\
         "{:.2f}".format(score.pp) + "pp\n"
