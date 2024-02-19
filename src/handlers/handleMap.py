40794764304db5a0d5c1648a089e0cb3f87c3ed3
MidVC <lutony040720@gmail.com>
2024-02-19 16:47:34 -0500

    support mods on handleMap

--- src/handlers/handleMap.py
+++ src/handlers/handleMap.py
@@ -1,15 +1,20 @@
 from ossapi import Ossapi, Beatmap, Mod
 
 def handleMap(api: Ossapi, mapID: int, mods:Mod) -> str :
+
     map = api.beatmap(beatmap_id=mapID)
+    diff = api.beatmap_attributes(beatmap_id=mapID, mods=mods, ruleset="osu").attributes
 
-    ans = "Map:https://osu.ppy.sh/beatmaps/" + str(mapID) + "\n"
+    ans = "Map:https://osu.ppy.sh/b/" + str(mapID) + "\n"
 
-    ans += "⭐" + "{:.2f}".format(map.difficulty_rating) + " | "
-    ans += "{:.0f}".format(map.bpm) + "bpm | "
-    ans += "AR: " + "{:.2f}".format(map.ar) + " | "
+    ans += "⭐" + "{:.2f}".format(diff.star_rating) + " | "
+    if (("DT" in mods.short_name()) or ("NC" in mods.short_name())):
+        ans += "{:.0f}".format(map.bpm * 1.5) + "bpm | "
+    else:
+        ans += "{:.0f}".format(map.bpm) + "bpm | "
+    ans += "AR: " + "{:.2f}".format(diff.approach_rate) + " | "
     ans += "CS: " + "{:.2f}".format(map.cs) + " | "
-    ans += "OD: " + "{:.2f}".format(map.accuracy) + " | "
+    ans += "OD: " + "{:.2f}".format(diff.overall_difficulty) + " | "
     ans += "HP: " + "{:.2f}".format(map.drain) + "\n"
 
     return ans
