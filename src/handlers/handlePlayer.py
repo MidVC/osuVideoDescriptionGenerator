c478a484f6b4e5c53776ba5d84d409796948eab1
MidVC <lutony040720@gmail.com>
2024-02-19 17:23:36 -0500

    implemented handlePlayer

--- src/handlers/handlePlayer.py
+++ src/handlers/handlePlayer.py
@@ -1,4 +1,15 @@
 from ossapi import Ossapi
 
 def handlePlayer(api: Ossapi, playerId: int) -> str :
-    return ""
+    user = api.user(user=playerId)
+    ans = "Player: https://osu.ppy.sh/u/"
+    ans += str(playerId) + "\n"
+
+    ans += "Username: " + user.username + "\n"
+
+    ans += "{:.2f}".format(user.statistics.pp) + "pp, "
+    ans += "#" + str(user.statistics.global_rank) + " global (#"
+    ans += str(user.statistics.country_rank) + " " + user.country_code + ")\n"
+
+    return ans
+    
